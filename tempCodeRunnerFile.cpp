895a67df72d39f736ce1c70a79dfb57548ebc077
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2021-10-12 12:35:07 +0530

    added

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,9 @@
+cout << "Enter the IP Address in Dotted Decimal Notation: " << endl;
+    cout << " Enter First Octet: ";
+    cin >> first;
+    cout << "Enter Second Octet: ";
+    cin >> second;
+    cout << "Enter Third Octet: ";
+    cin >> third;
+    cout << "Enter Fourth Octet: ";
+    cin >> fourth;
