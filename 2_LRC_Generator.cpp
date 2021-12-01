4a294b6440803f45db927101ff7cd860031f6dc5
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2021-12-01 18:54:46 +0530

    Update 2_LRC_Generator.cpp

--- 2_LRC_Generator.cpp
+++ 2_LRC_Generator.cpp
@@ -1,26 +1,26 @@
 /**
- * 
+ *
  * todo: OBJECTIVE = Program to generate LRC for any character
- * 
+ *
  * ! changes and addons to do in program:
- * todo: 
- * 
- * ! 
- * 
- * ! Program to 
- * 
- * ? initialization dt. 
- * ? last updated on dt. 
- * 
+ * todo:
+ *
+ * !
+ *
+ * ! Program to LRC Generator
+ *
+ * ? initialization dt.
+ * ? last updated on dt.
+ *
  * ? this program is made by SUDHANWA KAVEESHWAR....
- * 
- * ! **************** PROBLEMS ****************** 
- * ! 
- * 
+ *
+ * ! **************** PROBLEMS ******************
+ * !
+ *
  * * *************** OTHERS *******************
- * * 
- * 
-*/
+ * *
+ *
+ */
 
 #include <bits/stdc++.h>
 #include <iostream>
