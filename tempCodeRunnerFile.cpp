470f3858af28826661af10d43fa62497bdf0e19b
Sudhanwa Kaveeshwar <sudhanwa.kaveeshwar@gamil.com>
2021-12-06 15:48:53 +0530

    updated gitignorefile

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -3,30 +3,92 @@
 
 using namespace std;
 
-char Suggestion(int Host)
+string DottedDecimalToBinary(long int Decimal)
 {
-    if (Host <= pow(2, 24) - 2 && Host > pow(2, 16) - 2)
+    string s = "";
+
+    int Temp[8] = {0};
+
+    int i = 0;
+
+    while (Decimal > 0)
     {
-        return 'A';
+        Temp[i] = Decimal % 2;
+        Decimal /= 2;
+        i++;
     }
-    else if (Host <= pow(2, 16) - 2 && Host > pow(2, 8) - 2)
+
+    Reverse(Temp, Temp + 8);
+
+    for (int i = 0; i < 8; i++)
     {
-        return 'B';
+        s += std::to_string(Temp[i]);
     }
-    else if (Host <= pow(2, 8) - 2)
-    {
-        return 'C';
-    }
+
+    return s;
+}
+
+int NumberOfOne(string s)
+{
+    int OneCount = count(s.begin(), s.end(), '1');
+
+    return OneCount;
+}
+
+int NumberOfTwo(string s)
+{
+    int ZeroCount = count(s.begin(), s.end(), '0');
+
+    return ZeroCount;
 }
 
 int main()
 {
-    int HostNumber;
+    string s;
 
-    cout << "Enter number of host" << endl;
-    cin >> HostNumber;
+    long int First, Second, Third, Fourth;
 
-    cout << "Suggested IP address Class is :" << Suggestion(HostNumber) << endl;
+    cout << "Enter IP Address in dotted decimal notation" << endl;
+    cout << "Enter First Decimal Octet: ";
+    cin >> First;
+    cout << "Enter Second Decimal Octet: ";
+    cin >> Second;
+    cout << "Enter Third Decimal Octet: ";
+    cin >> Third;
+    cout << "Enter Fourth Decimal Octet: ";
+    cin >> Fourth;
+
+    string SFirst = DottedDecimalToBinary(First);
+    string SSecond = DottedDecimalToBinary(Second);
+    string SThird = DottedDecimalToBinary(Third);
+    string SFourth = DottedDecimalToBinary(Fourth);
+
+    int One, Zero;
+
+    if (First == 255)
+    {
+        if (Second == 255)
+        {
+            if (Third == 255)
+            {
+                One = NumberOfOne(SFourth);
+                Zero = 8 - One;
+            }
+            else
+            {
+                One = NumberOfOne(SThird);
+                Zero = 16 - One;
+            }
+        }
+        else
+        {
+            One = NumberOfOne(SSecond);
+            Zero = 24 - One;
+        }
+    }
+
+    cout << "Number of Subnets are: " << pow(2, One) << endl;
+    cout << "Number of hosts are: " << pow(2, Zero) << endl;
 
     return 0;
 }
